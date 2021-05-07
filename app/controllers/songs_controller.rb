b2224614dc03cf5afced527c1fc44cb9120303c7
Jesse Wenger <wengerj@gmail.com>
2021-05-07 01:22:51 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,7 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
-      if Artist.exists?(params[:artist_id])
-        @songs = Artist.find(params[:artist_id]).songs
-      else
-        redirect_to artists_path
-      end
+      Artist.exists?(params[:artist_id]) ? @songs = Artist.find(params[:artist_id]).songs : redirect_to artists_path
     else
       @songs = Song.all
     end
